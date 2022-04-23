45f621e3294584ff99b591a545815548eb07a8d6
noelvalent <nv.noelvalent@gamil.com>
2022-04-23 16:54:50 +0800

    Add views and urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,18 +1,30 @@
 from django.urls import path
 
-from shop.views import (
+from .views import (
     index,
     about_view,
-    news_view,
-    article_view,
     contact_view,
+    ArticleListView,
+    ArticleDetailView,
+    AnnounceListView,
+    AnnounceDetailView,
+    DealListView,
+    DealDetailView,
+    StoreListView,
+    StoreDetailView,
 )
 
 
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about_view, name='about'),
-    path('news/', news_view, name='news'),
-    path('article/', article_view, name='article'),
     path('contact/', contact_view, name='contact'),
+    path('article/', ArticleListView.as_view(), name='article-list'),
+    path('article/<int:id>/', ArticleDetailView.as_view(), name='article-detail'),
+    path('announce/', AnnounceListView, name='announce-list'),
+    path('announce/<int:id>/', AnnounceDetailView, name='announce-detail'),
+    path('deal/', DealListView.as_view(), name='deal-list'),
+    path('deal/<int:id>/', DealDetailView.as_view(), name='deal-detail'),
+    path('store/', StoreListView.as_view(), name='store-list'),
+    path('store/<int:id>/', StoreDetailView.as_view(), name='store-detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+)
+
+from shop.models import *
 
 
 # Create your views here.
@@ -17,17 +23,57 @@ def about_view(request):
     pass
 
 
-def store_view(request):
-    pass
-
-
-def news_view(request):
-    pass
-
-
-def article_view(request):
-    pass
-
-
 def contact_view(request):
     pass
+
+
+class StoreListView(ListView):
+    model = Store
+    paginate_by = 9
+    template_name = ''
+    context_object_name = 'stores'
+
+
+class StoreDetailView(DetailView):
+    model = Store
+    template_name = ''
+    context_object_name = 'store'
+
+
+class AnnounceListView(ListView):
+    model = Announce
+    paginate_by = 9
+    template_name = ''
+    context_object_name = 'announces'
+
+
+class AnnounceDetailView(DetailView):
+    model = Announce
+    template_name = ''
+    context_object_name = 'announce'
+
+
+class DealListView(ListView):
+    model = Deal
+    paginate_by = 9
+    template_name = ''
+    context_object_name = 'deals'
+
+
+class DealDetailView(DetailView):
+    model = Deal
+    template_name = ''
+    context_object_name = 'deal'
+
+
+class ArticleListView(ListView):
+    model = Article
+    paginate_by = 9
+    template_name = ''
+    context_object_name = 'articles'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = ''
+    context_object_name = 'article'
